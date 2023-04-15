681ef2fb4f1a77cb1c43c7a12644c3122653f84c
venkateshgopal28 <venkatesh_gopal@epam.com>
2023-04-15 21:41:35 +0530

    sample Rest Assured Test cases for GET

--- src/test/java/LearnApi.java
+++ src/test/java/LearnApi.java
@@ -2,13 +2,28 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class LearnApi {
 
     @Test
-    public void Test1()
+    public void Verifystatuscode()
     {
-        given().baseUri("https://reqres.in").when().get("/api/users").then().assertThat().statusCode(200);
+        given().baseUri("https://reqres.in").when().get("/api/users/8").then().assertThat().statusCode(200);
+    }
+
+    @Test
+    public void Verifyresponse()
+    {
+        given().baseUri("https://reqres.in").when().get("/api/users/8").then().assertThat().statusCode(200).body("data.first_name",equalTo("Lindsay"));
+        given().baseUri("https://reqres.in").when().get("/api/users/8").then().assertThat().statusCode(200).body("data.last_name",equalTo("Ferguson"));
+        given().baseUri("https://reqres.in").when().get("/api/users/8").then().assertThat().statusCode(200).body("data.avatar",equalTo("https://reqres.in/img/faces/8-image.jpg"));
+    }
+
+    @Test
+    public void ContentType()
+    {
+        given().baseUri("https://reqres.in").when().get("/api/users/8").then().assertThat().statusCode(200).contentType("application/json; charset=utf-8");
 
     }
 }
